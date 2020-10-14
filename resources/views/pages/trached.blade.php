d6113afe11840d4f31bf6df663f0f1f9c2870c22
Amine Hmidouche <amine.102.hmidouche@gmail.com>
2020-10-14 21:22:55 +0200

    ticke1

--- resources/views/pages/trached.blade.php
+++ resources/views/pages/trached.blade.php
@@ -1,8 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
 <h1>trached product</h1>
 
 @foreach ($produits as $produit)
-
+<div style="align-content: left">
 <h1>{{$produit->title}}</h1>
 <p>{{$produit->description}}</p>
-    
+    </div>
 @endforeach
+@endsection
